3323532a2b1c041123f6e47c74ccb1d735c8a0ea
Arhaan <113068699+arhaankk@users.noreply.github.com>
2025-02-04 13:59:47 -0800

    added basic physics + prefabs

---
+++ Assets/BallController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+    private Rigidbody ballRB;
+    private bool isBallLaunched;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ballRB = GetComponent<Rigidbody>();
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+    }
+
+    private void LaunchBall()
+    {
+        if (isBallLaunched) return;
+
+        isBallLaunched = true;
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
