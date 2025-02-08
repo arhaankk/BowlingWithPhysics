611c9d405eaca580493bdad77bd31cd6231ebd72
Arhaan <113068699+arhaankk@users.noreply.github.com>
2025-02-07 17:21:09 -0800

    Moved to Cinemachine

--- Assets/Gutter.cs
+++ Assets/Gutter.cs
@@ -4,11 +4,12 @@ public class Gutter : MonoBehaviour
 {
     private void OnTriggerEnter (Collider triggeredBody)
     {
+        Debug.Log("collision happened!");
         Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
         float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
         ballRigidBody.linearVelocity = Vector3.zero;
         ballRigidBody.angularVelocity = Vector3.zero;
-        ballRigidBody.AddForce(transform.forward * velocityMagnitude, ForceMode.VelocityChange);
+        ballRigidBody.AddForce(Vector3.forward * velocityMagnitude, ForceMode.VelocityChange);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
